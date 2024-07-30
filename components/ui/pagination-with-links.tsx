0f136719db1ad40ef220025c0fdd5d1c856068ba
Bryan Eaton <bryaneaton13@gmail.com>
2024-07-30 16:12:55 -0400

    Add jsdoc comment

--- components/ui/pagination-with-links.tsx
+++ components/ui/pagination-with-links.tsx
@@ -25,6 +25,18 @@ export interface PaginationWithLinksProps {
   pageSearchParam?: string;
 }
 
+/**
+ * Navigate with Nextjs links (need to update your own `pagination.tsx` to use Nextjs Link)
+ * 
+ * @example
+ * ```
+ * <PaginationWithLinks
+    page={1}
+    pageSize={20}
+    totalCount={500}
+  />
+ * ```
+ */
 export function PaginationWithLinks({
   pageSizeSelectOptions,
   pageSize,
